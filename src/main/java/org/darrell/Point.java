5317b95ac282e3ca7001effa069dfdad7c0a3f7e
darrellwong <darrellwong1@gmail.com>
2022-02-21 21:40:32 -0600

    Fixing Constructors

--- src/main/java/org/darrell/Point.java
+++ src/main/java/org/darrell/Point.java
@@ -1,6 +1,13 @@
 package org.darrell;
 
 public class Point {
+    private int x;
+    private int y;
 
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
 
 }
--- src/main/java/org/darrell/Rectangle.java
+++ src/main/java/org/darrell/Rectangle.java
@@ -8,6 +8,11 @@ public class Rectangle {
      * @param r Rectangle
      * @return True if this rectangle overlaps rectangle r with positive area
      */
+    public Rectangle(Point p1, Point p2)
+    {
+
+    }
+
     public boolean isOverlap(Rectangle r) {
         // FIXME: Implement this
         return true;
